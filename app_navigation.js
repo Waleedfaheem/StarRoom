ea0f8c8db9df2c03409943aef5ee82cd38cbc1ff
StarRoom <rtiktok951@gmail.com>
2026-07-17 19:53:51 +0500

    Add navigation functions for login, register, and more
    Implement navigation functions for various pages.
    
    Signed-off-by: StarRoom <rtiktok951@gmail.com>

---
+++ app_navigation.js
@@ -0,0 +1,50 @@
+// StarRoom Navigation System
+
+
+function goLogin(){
+
+window.location.href="login.html";
+
+}
+
+
+function goRegister(){
+
+window.location.href="register.html";
+
+}
+
+
+function goHome(){
+
+window.location.href="home.html";
+
+}
+
+
+function goRooms(){
+
+window.location.href="room-list.html";
+
+}
+
+
+function goWallet(){
+
+window.location.href="wallet.html";
+
+}
+
+
+function goProfile(){
+
+window.location.href="profile.html";
+
+}
+
+
+function goVoiceRoom(){
+
+window.location.href="voice_room.html";
+
+<script src="app_navigation.js"></script>
