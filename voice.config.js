870d5c117d010c674781c8b2af910b3d0c0dada3
StarRoom <rtiktok951@gmail.com>
2026-07-17 21:23:00 +0500

    Create voice.config.js
    Signed-off-by: StarRoom <rtiktok951@gmail.com>

---
+++ voice.config.js
@@ -0,0 +1,19 @@
+// StarRoom Voice Configuration
+
+
+const voiceConfig = {
+
+    servers: [
+        {
+            urls: [
+                "stun:stun.l.google.com:19302"
+            ]
+        }
+    ]
+
+};
+
+
+// Room ID
+
+const roomID = "default";
